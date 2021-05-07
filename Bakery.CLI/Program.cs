934c605497637eb8379e114ebd5f972092eaa6d3
Min Chang <minchangmhc@gmail.com>
2021-05-07 15:08:05 -0700

    Add UI functionality that asks for bread.

--- Bakery.CLI/Program.cs
+++ Bakery.CLI/Program.cs
@@ -9,7 +9,9 @@ namespace Bakery.CLI
     {
       Console.WriteLine("Welcome to Pierre's Bakery, each loaf of bread is $5 and each pastry is $2. Specials: Buy two or more loafs of bread and get one free! Buy 3 or more pastries and get one half off!");
       Console.Write("How many loafs of bread do you want?");
-      string userInput = Console.ReadLine();
+      string breadInput = Console.ReadLine();
+      int breadPrice = int.Parse(breadInput);
+      Console.WriteLine($"Your total is ${Bread.Loaf(breadPrice)}");
 
     }
   }
