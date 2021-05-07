112c8704904c16b83ee1a828e7a1ca80aace5e7f
Min Chang <minchangmhc@gmail.com>
2021-05-07 15:23:14 -0700

    Add UI logic for pastry class.

--- Bakery.CLI/Program.cs
+++ Bakery.CLI/Program.cs
@@ -7,12 +7,16 @@ namespace Bakery.CLI
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery, each loaf of bread is $5 and each pastry is $2. Specials: Buy two or more loafs of bread and get one free! Buy 3 or more pastries and get one half off!");
-      Console.Write("How many loafs of bread do you want?");
+      Console.WriteLine("Welcome to Pierre's Bakery, each loaf of bread is $5 and each pastry is $2.");
+      Console.WriteLine("Bread Specials: Buy two or more loafs of bread and get one free!");
+      Console.WriteLine("Pastry Specials: Buy 3 or more pastries and get one half off!");
+      Console.Write("How many loafs of bread do you want? ");
       string breadInput = Console.ReadLine();
       int breadPrice = int.Parse(breadInput);
-      Console.WriteLine($"Your total is ${Bread.Loaf(breadPrice)}");
-
+      Console.Write("How many pastries do you want? ");
+      string pastryInput = Console.ReadLine();
+      int pastryPrice = int.Parse(pastryInput);
+      Console.WriteLine($"Your total is ${Bread.Loaf(breadPrice) + Pastry.PricePastry(pastryPrice)}.");
     }
   }
 }
