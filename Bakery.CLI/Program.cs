74a9dd4fc9fdd545d9144f2d480a7e82cc822794
Min Chang <minchangmhc@gmail.com>
2021-05-07 13:44:44 -0700

    Add boilerplate for UI logic.

---
+++ Bakery.CLI/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using Bakery;
+
+namespace Bakery.CLI
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery, each loaf of bread is $5 and each pastry is $2. Specials: Buy two or more loafs of bread and get one free! Buy 3 or more pastries and get one half off!");
+      Console.Write("How many loafs of bread do you want?");
+      string userInput = Console.ReadLine();
+
+    }
+  }
+}
